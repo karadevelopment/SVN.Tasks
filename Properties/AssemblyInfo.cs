80f6c2957ed9e79ba3a8bff2a00f7c834f5592e0
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:57:43 +0100

    1.0.1.0

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SVN.Tasks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SVN Development")]
+[assembly: AssemblyProduct("SVN.Tasks")]
+[assembly: AssemblyCopyright("Copyright © Muhammed Kara 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5cccf2b2-0f68-4996-8c3d-3b5569ccd9f3")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
---
+++ TaskContainer.cs
@@ -0,0 +1,47 @@
+﻿using SVN.Debug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SVN.Tasks
+{
+    public static class TaskContainer
+    {
+        private static int Counter { get; set; }
+        private static List<int> List { get; } = new List<int>();
+        private static CancellationTokenSource CancellationTokenSource { get; } = new CancellationTokenSource();
+
+        public static bool Any
+        {
+            get => TaskContainer.List.Any();
+        }
+
+        public static Task Run(Action action)
+        {
+            var id = ++TaskContainer.Counter;
+            TaskContainer.List.Add(id);
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception e)
+                {
+                    Logger.Write(e.GetType().Name, e.Message, e.StackTrace);
+                }
+                TaskContainer.List.Remove(id);
+            }, TaskContainer.CancellationTokenSource.Token);
+
+            return task;
+        }
+
+        public static void Abort()
+        {
+            TaskContainer.CancellationTokenSource.Cancel();
+        }
+    }
+}
